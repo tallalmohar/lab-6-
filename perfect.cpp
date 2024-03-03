8f4cb019e33c49a5a380b86369217d54d0eda167
Tallal Mohar <67089863+tallalmohar@users.noreply.github.com>
2024-03-02 20:42:23 -0800

    first lab commit

---
+++ perfect.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+
+bool isProperFactor(int num, int divisor){
+    if((divisor < num) && ((num%divisor) == 0)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int sumOfProperFactors(int n){
+
+
+}
+
+int main(){
+
+   
+    return 0;
+    
+}
+
